a3139b223221405c59441452f8304124831a23cb
Sonu <bilawal.jabbar.786@gmail.com>
2021-12-15 22:18:55 +0500

    cont

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -34,6 +34,10 @@ $gs = \App\GeneralSettings::find(1);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <link rel="icon" type="image/png" href="{{asset($gs->logo)}}" sizes="32x32">
+    <link rel="apple-touch-icon" href="{{asset($gs->logo)}}">
+
     <!-- /STYLES -->
     <style>
 
